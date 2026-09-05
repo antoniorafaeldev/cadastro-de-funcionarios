f85ba07b4983159a38a3c064cffdf099472704c4
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-09-05 18:39:47 -0300

    style: worker register page

---
+++ worker-register.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar um Funcionário</title>
+    <link rel="stylesheet" href="./css/worker-register.css">
+</head>
+
+<body>
+    <header class="header">
+        <nav>
+            <ul class="links">
+                <li><a href="" class="link">Cadastrar Funcionário</a></li>
+                <li><a href="" class="link">Visualizar Funcionários</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="register">
+            <h1 class="title">Cadastrar um Funcionário</h1>
+            <form action="" method="post" id="form">
+                <label for="">Nome Completo</label>
+                <input class="input" type="text" name="name" id="name" required>
+
+                <label for="email">Email</label>
+                <input class="input" type="email" name="email" id="email" required>
+                <p class="error-message" id="email-error">Por favor, adicione um endereço de email válido</p>
+
+                <label for="role">Cargo</label>
+                <input class="input" type="text" name="role" id="role" required>
+
+                <label for="wage">Salário</label>
+                <input class="input" type="number" name="wage" id="wage" required>
+
+                <input class="submit" type="submit" value="Cadastrar">
+            </form>
+        </section>
+
+    </main>
+
+    <script src="./js/form-validation.js"></script>
+</body>
+
+</html>
