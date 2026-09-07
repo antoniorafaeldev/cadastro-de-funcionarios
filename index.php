3bd6ceccb938ee979bca381ba0a9293d2400b066
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-09-07 17:32:47 -0300

    feat: render for empty workers

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 require_once("db_connection.php");
+include("./utils/html_render.php");
 
 $sql = "SELECT * FROM Funcionarios";
 $result = mysqli_query($db_connection, $sql);
 $total_workers = mysqli_num_rows($result);
+date_default_timezone_set('America/Sao_Paulo');
+$pattern = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
 ?>
 <!DOCTYPE html>
@@ -37,37 +40,25 @@ $total_workers = mysqli_num_rows($result);
             <div class="workers-grid">
                 <?php
                     while ($row = mysqli_fetch_assoc($result)) {
+                        $id = htmlspecialchars($row["id"], ENT_QUOTES, 'UTF-8');
                         $name = htmlspecialchars($row["nome"], ENT_QUOTES, 'UTF-8');
                         $email = htmlspecialchars($row["email"], ENT_QUOTES, 'UTF-8');
                         $role = htmlspecialchars($row["cargo"], ENT_QUOTES, 'UTF-8');
                         $wage = $row["salario"];
                         $reg_date = $row["data_registro"];
 
-                        date_default_timezone_set('America/Sao_Paulo');
-                        $pattern = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-
                         $formatted_wage = numfmt_format_currency($pattern, $wage, "BRL");
                         $formatted_reg_date = date("d/m/Y H:i:s", strtotime($reg_date));
 
-                        echo "<article class='worker-card'>";
-                        echo "<div class='worker-card__header'>";
-                        echo "<span class='worker-badge'>Funcionário</span>";
-                        echo "<h2 class='worker-name'>$name</h2>";
-                        echo "</div>";
-                        echo "<dl class='worker-info'>";
-                        echo "<div><dt>Cargo</dt><dd>$role</dd></div>";
-                        echo "<div><dt>Salário</dt><dd>$formatted_wage</dd></div>";
-                        echo "<div><dt>Email</dt><dd>$email</dd></div>";
-                        echo "<div><dt>Cadastro</dt><dd>$formatted_reg_date</dd></div>";
-                        echo "</dl>";
-                        echo "<div class='buttons'>";
-                        echo "<button>Editar Funcionário</button>";
-                        echo "<button>Deletar Funcionário</button>";
-                        echo "</div>";
-                        echo "</article>";
+                        render_worker($name, $role, $email, $formatted_wage, $formatted_reg_date);
                     }
                     ?>
             </div>
+        </section>
+        <?php else: ?>
+        <section>
+            <h2>Nenhum funcionário cadastrado!</h2>
+            <a href="./worker-register.php" class="link">Cadastrar o primeiro funcionário</a>
         </section>
         <?php endif ?>
     </main>
